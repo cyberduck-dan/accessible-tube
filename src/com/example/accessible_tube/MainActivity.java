2a3b020107cd96f6294e7aa390922b488e1afd0c
Dmitriy <dima.blinkov@gmail.com>
2013-04-28 12:06:21 +0100

    - added Location call

--- src/com/example/accessible_tube/MainActivity.java
+++ src/com/example/accessible_tube/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.accessible_tube;
 
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -20,8 +25,30 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	
+	
 	public void ButOnClickXml(View v) {
-	    
+		EditText editText1 = (EditText) findViewById(R.id.editText1);		
+		
+		double lat;
+		double lon;
+		
+		try{
+			Location loc = getLocation();
+			lat = loc.getLatitude();
+			lon = loc.getLongitude();
+			editText1.setText("Lat - " + Double.toString(lat) + ", Lon - " + Double.toString(lon));
+		}catch(Exception e){			
+			editText1.setText("Could not access the location service");
+		}
+		
 	}
-
+	
+	private Location getLocation(){
+		 // Get the location manager
+		 LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		 Criteria criteria = new Criteria();
+		 String bestProvider = locationManager.getBestProvider(criteria, false);
+		 return locationManager.getLastKnownLocation(bestProvider);	
+	}
 }
